517305d9cb0a39512dc4f566e3a9d43d12117edb
OlymbusFrance <ranojidossradhakrishnan@gmail.com>
2022-08-16 03:17:02 +0530

    Ferrari adding method 5,6

--- src/test/java/github/ClassA.java
+++ src/test/java/github/ClassA.java
@@ -10,12 +10,12 @@ public class ClassA {
 		System.out.println("GST National Conference at Chennai");
 	}
 
-	private void m3() {
-		System.out.println("Modi visit to Chennai");
+	private void m5() {
+		System.out.println("Ukrane War still exists");
 	}
 
-	private void m4() {
-		System.out.println("Nirmala Seetha Raman visited Chennai");
+	private void m6() {
+		System.out.println("China landed in Srilanka");
 	}
 	public static void main(String[] args) {
 
