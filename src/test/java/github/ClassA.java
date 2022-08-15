01f2645adf4f55b4e451ac83c773a2d5ef64419b
OlymbusFrance <ranojidossradhakrishnan@gmail.com>
2022-08-16 01:15:39 +0530

    ClassA initial

---
+++ src/test/java/github/ClassA.java
@@ -0,0 +1,16 @@
+package github;
+
+public class ClassA {
+
+	private void m1() {
+		System.out.println("Chess Olymbiad conducted at Mahabalipuram");
+
+	}
+	private void m2() {
+		System.out.println("GST National Conference at Chennai");
+	}
+	public static void main(String[] args) {
+
+	}
+
+}
