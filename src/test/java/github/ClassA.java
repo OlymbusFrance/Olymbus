8e4130b66e5dbc301fa8d088707281069065f6b5
OlymbusFrance <ranojidossradhakrishnan@gmail.com>
2022-08-16 02:45:31 +0530

    Branch1 Mclauren updated

--- src/test/java/github/ClassA.java
+++ src/test/java/github/ClassA.java
@@ -9,6 +9,14 @@ public class ClassA {
 	private void m2() {
 		System.out.println("GST National Conference at Chennai");
 	}
+
+	private void m3() {
+		System.out.println("Modi visit to Chennai");
+	}
+
+	private void m4() {
+		System.out.println("Nirmala Seetha Raman visited Chennai");
+	}
 	public static void main(String[] args) {
 
 	}
